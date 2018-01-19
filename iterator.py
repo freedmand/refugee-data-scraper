1b7062f65f818cebf6886be41a0a1e0dde68be27
freedmand <freedmand@gmail.com>
2018-01-19 11:01:57 -0800

    Added an iterate that cleans the data and returns it with a structured header row

--- iterator.py
+++ iterator.py
@@ -2,6 +2,7 @@ import constants
 import csv
 import itertools
 import os
+import datetime
 
 class ImmigrationPatter(object):
   def __init__(self, day, originCountry, destState, destCounty, count):
@@ -19,10 +20,24 @@ class ImmigrationPatter(object):
     )
 
 def extractDate(date, prefix = "From: "):
-  assert(date.startswith(prefix)), 'Invalid date format'
+  assert(date.startswith(prefix)), 'Invalid date format: %s' % date
+  # Trim the prefix.
+  date = date[len(prefix):]
+  # Parse the date.
+  return datetime.datetime.strptime(date, '%d %b %Y')
 
+def cleanRow(row):
+  day = extractDate(row[0])
+  state = row[2]
+  country = row[5]
+  city = row[7]
+  count = row[8]
+  return [day, country, state, city, count]
 
 def readAllRows():
+  # Return the header row.
+  yield ['Day', 'Country', 'State', 'City', 'Count']
+
   for fn in os.listdir(constants.EXTRACTED_DIR):
     # Skip any non-CSV file.
     if not fn.endswith('.csv'): continue
@@ -31,14 +46,18 @@ def readAllRows():
     with open(fullPath, 'rb') as csvfile:
       reader = csv.reader(csvfile, delimiter=',', quotechar='"')
 
-      previousRow = None
+      # Only return rows after finding a valid header.
+      foundHeader = False
       for row in reader:
-        if row and row[0].startswith('From:'):
-          if previousRow != None:
-            yield previousRow
-            previousRow = None
-        else:
-          previousRow = row
+        if foundHeader:
+          if row and row[0].startswith('From:'):
+            yield cleanRow(row)
+          else:
+            # If the row is not valid, stop consuming rows after that.
+            foundHeader = False
+        # The valid header we're looking for in the data.
+        if row == ['Textbox87','Textbox82','nat_definition4','region_name_3','textbox37','Category3','textbox39','Assur_DestinationCity1','Cases3','Cases4']:
+          foundHeader = True
 
 import csv
 with open('combined_out.csv', 'wb') as csvfile:
